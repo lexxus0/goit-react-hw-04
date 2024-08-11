80cd84a67daae3e3f4f968f6355f64efde3c54fd
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2024-08-11 17:26:43 +0300

    galleryFix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,23 +1,11 @@
 import ImageCard from "../ImageCard/ImageCard";
 import css from "./ImageGallery.module.css";
 
-import { useState } from "react";
-
 const ImageGallery = ({ images, onImgClick }) => {
-  const [hoveredIndex, setHoveredIndex] = useState(null);
   return (
     <ul className={css.galleryList}>
-      {images.map((image, index) => (
-        <li
-          key={image.id}
-          className={css.galleryItem}
-          onMouseEnter={() => setHoveredIndex(index)}
-          onMouseLeave={() => setHoveredIndex(null)}
-          style={{
-            opacity: hoveredIndex !== null && hoveredIndex !== index ? 0.6 : 1,
-            transform: hoveredIndex === index ? "scale(1.1)" : "scale(1)",
-          }}
-        >
+      {images.map((image) => (
+        <li key={image.id} className={css.galleryItem}>
           <ImageCard
             small={image.urls.small}
             desc={image.description}
